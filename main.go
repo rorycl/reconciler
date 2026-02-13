45b5e7ed3f0a26dced5ac084699cfbd9c7f77071
rorycl <rorycl@campbell-lange.net>
2026-02-13 16:28:44 +0000

    main: update temporary entry point for structured logging

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 	"reconciler/config"
 	"reconciler/db"
@@ -14,29 +15,35 @@ import (
 // This is a temporary function for development.
 func runServer() {
 
-	logger := log.Default()
-
 	if len(os.Args) != 2 {
 		fmt.Println("Please provide the configuration yaml file as an argument")
 		os.Exit(1)
 	}
 	yamlFile := os.Args[1]
 
+	// Load the configuration file.
 	cfg, err := config.Load(yamlFile)
 	if err != nil {
 		fmt.Println("configuration error:", err)
 		os.Exit(1)
 	}
 
+	// Configure Logging.
+	logger := slog.Default()
+	if cfg.Web.DevelopmentMode {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
+	// Mount the sql filesystem.
 	sqlFS, err := internal.NewFileMount("sql", db.SQLEmbeddedFS, "")
 	if err != nil {
 		fmt.Println("could not mount sql fs:", err)
 		os.Exit(1)
 	}
 
+	// Create the database connection.
 	// accountCodes := "^(53|55|57)"
 	accountCodes := cfg.DonationAccountCodesRegex()
-
 	thisDB, err := db.NewConnection(cfg.DatabasePath, "", accountCodes)
 	if err != nil {
 		fmt.Println("database setup error", err)
@@ -50,6 +57,7 @@ func runServer() {
 
 	}
 
+	// Mount the web static resources and template filesystems.
 	staticFS, err := internal.NewFileMount("static", web.StaticEmbeddedFS, "")
 	if err != nil {
 		fmt.Println("static file mount error", err)
@@ -61,10 +69,11 @@ func runServer() {
 		os.Exit(1)
 	}
 
-	// testing data start and end dates
+	// Setup some start and end dates for testing.
 	startDate := time.Date(2025, 4, 1, 0, 0, 0, 0, time.UTC)
 	endDate := time.Date(2027, 3, 31, 0, 0, 0, 0, time.UTC)
 
+	// Configure and launch the web server.
 	webApp, err := web.New(logger, cfg, thisDB, staticFS, templatesFS, startDate, endDate)
 	if err != nil {
 		log.Fatal(err)
