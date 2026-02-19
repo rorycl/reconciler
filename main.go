19f9a35a17f33867fa48a43689f51b8b377d0240
rorycl <rorycl@campbell-lange.net>
2026-02-19 23:01:12 +0000

    main: refine temporary entry point

--- main.go
+++ main.go
@@ -9,10 +9,8 @@ import (
 	"reconciler/db"
 	"reconciler/internal"
 	"reconciler/web"
-	"time"
 )
 
-// This is a temporary function for development.
 func runServer() {
 
 	if len(os.Args) != 2 {
@@ -34,27 +32,12 @@ func runServer() {
 		slog.SetLogLoggerLevel(slog.LevelInfo)
 	}
 
-	// Mount the sql filesystem.
-	sqlFS, err := internal.NewFileMount("sql", db.SQLEmbeddedFS, "")
-	if err != nil {
-		fmt.Println("could not mount sql fs:", err)
-		os.Exit(1)
-	}
-
 	// Create the database connection.
-	// accountCodes := "^(53|55|57)"
 	accountCodes := cfg.DonationAccountCodesRegex()
 	thisDB, err := db.NewConnection(cfg.DatabasePath, "", accountCodes, logger)
 	if err != nil {
 		fmt.Println("database setup error", err)
 		os.Exit(1)
-	}
-	// Load the schema definitions.
-	if err := thisDB.InitSchema(sqlFS, "schema.sql"); err != nil {
-		_ = thisDB.Close()
-		fmt.Println("database schema error", err)
-		os.Exit(1)
-
 	}
 
 	// Mount the web static resources and template filesystems.
@@ -69,12 +52,8 @@ func runServer() {
 		os.Exit(1)
 	}
 
-	// Setup some start and end dates for testing.
-	startDate := time.Date(2025, 4, 1, 0, 0, 0, 0, time.UTC)
-	endDate := time.Date(2027, 3, 31, 0, 0, 0, 0, time.UTC)
-
 	// Configure and launch the web server.
-	webApp, err := web.New(logger, cfg, thisDB, staticFS, templatesFS, startDate, endDate)
+	webApp, err := web.New(logger, cfg, thisDB, staticFS, templatesFS)
 	if err != nil {
 		log.Fatal(err)
 	}
