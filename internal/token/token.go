c2c33f9faa044e51661f41de60edd45f137c07a4
rorycl <rorycl@campbell-lange.net>
2026-02-20 10:52:05 +0000

    token: add xero/salesforce oauth2 token management to internal

---
+++ internal/token/token.go
@@ -0,0 +1,177 @@
+package token
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+// TokenType is the type of an OAuth2 decorated Token.
+type TokenType int
+
+const (
+	NoneToken TokenType = iota
+	XeroToken
+	SalesforceToken
+)
+
+var tokenName = map[TokenType]string{
+	NoneToken:       "invalid",
+	XeroToken:       "xero-token",
+	SalesforceToken: "salesforce-token",
+}
+
+// String returns the TokenType name string.
+func (tt TokenType) String() string {
+	return tokenName[tt]
+}
+
+// ExtendedToken is an OAuth2 token with additional information.
+type ExtendedToken struct {
+	Type        TokenType     `json:"type"`
+	Token       *oauth2.Token `json:"token"`
+	InstanceURL string        `json:"instance_url"` // only relevant to Salesforce Tokens
+}
+
+// NewExtendedToken creates a new ExtendedToken, running any ancillary checks and/or
+// fixups that might be necessary. Presently the Salesforce token requires the
+// instance_url to be extracted, and the expiry time set manually.
+func NewExtendedToken(typer TokenType, token *oauth2.Token) (*ExtendedToken, error) {
+	if token == nil {
+		return nil, errors.New("nil token received")
+	}
+	switch typer {
+	case XeroToken, SalesforceToken:
+	default:
+		return nil, errors.New("invalid token type received")
+	}
+
+	et := &ExtendedToken{
+		Type:  typer,
+		Token: token,
+	}
+
+	if typer == SalesforceToken {
+		err := et.fixSalesForceToken()
+		if err != nil {
+			return nil, fmt.Errorf("could not fix new salesforce token: %w", err)
+		}
+	}
+
+	return et, nil
+}
+
+// IsValid checks if the token is valid or if a refresh token exists to get a new token.
+// Tokens that expire after the expirationDuration will be considered invalid. This is
+// on the assumption that the validity period of tokens AND refresh tokens is known.
+func (et *ExtendedToken) IsValid(expirationDuration time.Duration) bool {
+	if et.Token == nil {
+		return false
+	}
+	if et.Token.Expiry.IsZero() {
+		return false
+	}
+	projectedExpiry := time.Now().UTC().Add(-1 * expirationDuration)
+	if !et.Token.Expiry.After(projectedExpiry) {
+		return false
+	}
+	return et.Token.Valid() || et.Token.RefreshToken != ""
+}
+
+// fixSalesforceTokenExpiry fixes the "IsZero" expiry of Saleforce's tokens.
+// sf tokens are something like the following. Annoyingly, the expiration time is set to
+// the Go zero time. This causes the TokenIsValid check to fail.
+//
+//	&oauth2.Token{
+//		AccessToken:"xxxx",
+//		TokenType:"Bearer",
+//		RefreshToken:"yyy",
+//		Expiry:time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+//		ExpiresIn:0,
+//		raw:map[string]interface {}{
+//			"access_token":"xxx",
+//			"id":"https://login.salesforce.com/id/00DxxxxxxA1/uuuuuuuuuuuuuuuuuu",
+//			"instance_url":"https://orgfarm-xxxxxxxxxx-dev-ed.develop.my.salesforce.com",
+//			"issued_at":"1771257689412",
+//			"refresh_token":"yyy",
+//			"scope":"refresh_token api",
+//			"signature":"zzz",
+//			"token_type":"Bearer"
+//		},
+//		expiryDelta:0,
+//	}
+func (et *ExtendedToken) fixSalesforceTokenExpiry() error {
+	if et == nil {
+		return errors.New("nil token in fixSalesforceTokenExpiry")
+	}
+	// Salesforce sends issued_at in milliseconds as a string
+	issuedAtStr, ok := et.Token.Extra("issued_at").(string)
+	if !ok || issuedAtStr == "" {
+		return errors.New("no issued_at found in salesforce token")
+	}
+	ms, err := strconv.ParseInt(issuedAtStr, 10, 64)
+	if err != nil {
+		return fmt.Errorf("could not parse salesforce issuedAtString: %w", err)
+	}
+	// Set Expiry.
+	const sessionLength = 2 * time.Hour
+	et.Token.Expiry = time.UnixMilli(ms).Add(sessionLength)
+	return nil
+}
+
+// setSalesforceInstanceURL retrieves the "instance_url" from a salesforce token. See
+// https://help.salesforce.com/s/articleView?id=xcloud.remoteaccess_oauth_client_credentials_flow.htm&type=5
+func (et *ExtendedToken) setSalesforceInstanceURL() error {
+	if et == nil {
+		return errors.New("nil token in setSalesforceInstanceURL")
+	}
+	var ok bool
+	et.InstanceURL, ok = et.Token.Extra("instance_url").(string)
+	if !ok || et.InstanceURL == "" {
+		return errors.New("no instance_url found in salesforce token")
+	}
+	return nil
+}
+
+// fixSalesForceToken wraps up various Salesforce Token fixer functions.
+func (et *ExtendedToken) fixSalesForceToken() error {
+	err := et.fixSalesforceTokenExpiry()
+	if err != nil {
+		return err
+	}
+	return et.setSalesforceInstanceURL()
+}
+
+// ReuseOrRefresh attempts to use or refresh an ExtendedToken using the provided context
+// and oauth2.Config. The config.TokenSource func automatically refreshes tokens when
+// needed. The function returns whether refreshing occurred and any error.
+func (et *ExtendedToken) ReuseOrRefresh(ctx context.Context, config *oauth2.Config) (bool, error) {
+
+	var refreshed bool
+
+	tok := config.TokenSource(ctx, et.Token)
+	possibleNewToken, err := tok.Token()
+	if err != nil {
+		return refreshed, fmt.Errorf("could not reuse or refresh token: %w", err)
+	}
+
+	// Check if refreshing occured. If not, return early.
+	if possibleNewToken.AccessToken == et.Token.AccessToken {
+		return refreshed, nil
+	}
+	refreshed = true
+	et.Token = possibleNewToken
+	fmt.Printf("possible New Token %#v\n", possibleNewToken)
+
+	if et.Type == SalesforceToken {
+		if err := et.fixSalesForceToken(); err != nil {
+			return refreshed, fmt.Errorf("could not fix reused/new salesforce token: %w", err)
+		}
+	}
+	return refreshed, nil
+
+}
---
+++ internal/token/token_test.go
@@ -0,0 +1,245 @@
+package token
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+
+	"reconciler/config"
+
+	"golang.org/x/oauth2"
+)
+
+// duration generates a time.Duration for test purposes.
+func duration(t *testing.T, s string) time.Duration {
+	t.Helper()
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return d
+}
+
+// tokenPrinter prints out a token, helpful for debugging.
+func tokenPrinter(t *oauth2.Token) string {
+	if t == nil {
+		return ""
+	}
+	return fmt.Sprintf("type: %s\nexpiry: %v\nrefresh: %v\n",
+		t.Type(),
+		t.Expiry,
+		t.RefreshToken,
+	)
+}
+
+// createFSConfig creates a salesforce configuration for tests.
+func createSFConfig(t *testing.T, callbackURL, serverURL string) config.SalesforceConfig {
+	t.Helper()
+	return config.SalesforceConfig{
+		LoginDomain:          serverURL,
+		ClientID:             "my-client-id",
+		ClientSecret:         "my-client-secret",
+		TokenTimeoutDuration: duration(t, "8h"),
+		OAuth2Config: &oauth2.Config{
+			RedirectURL: callbackURL,
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  fmt.Sprintf("%s/oauth2/authorize", serverURL),
+				TokenURL: fmt.Sprintf("%s/oauth2/token", serverURL),
+			},
+			Scopes: []string{"api", "refresh_token"},
+		},
+	}
+}
+
+// TestTokenNotExpired tests that a (Salesforce) valid, non-expired token does not
+// trigger a token refresh.
+func TestTokenNotExpired(t *testing.T) {
+
+	const instanceURL = "https://instance-url-example"
+
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	// Salesforce API authorization endpoint.
+	mux.HandleFunc("/oauth2/authorize", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if got, want := authHeader, "Bearer valid-token-123"; got != want {
+			t.Errorf("Incorrect Authorization header got %q want %q", got, want)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		_, _ = fmt.Fprintf(w, `[{"instance_url": "%s"}]`, instanceURL)
+	})
+
+	// Salesforce API /oauth2/token refresh endpoint (which should not
+	// be called in this test).
+	mux.HandleFunc("/oauth2/token", func(w http.ResponseWriter, r *http.Request) {
+		t.Fatal("token refresh endpoint was called unexpectedly")
+	})
+
+	validToken := &ExtendedToken{
+		Type:        SalesforceToken,
+		InstanceURL: instanceURL,
+		Token: &oauth2.Token{
+			AccessToken: "valid-token-123",
+			Expiry:      time.Now().Add(1 * time.Hour), // not expired
+		},
+	}
+
+	if !validToken.IsValid(duration(t, "8h")) {
+		t.Fatalf("token in %#v should be valid", validToken)
+	}
+
+	cfg := &config.Config{
+		Salesforce: createSFConfig(t, "/callback/sf", server.URL),
+	}
+
+	refreshed, err := validToken.ReuseOrRefresh(context.Background(), cfg.Salesforce.OAuth2Config)
+	if err != nil {
+		t.Fatalf("ReuseOrRefresh returned an error: %v", err)
+	}
+	if refreshed == true {
+		t.Fatal("refresh unexpectedly returned true")
+	}
+	if got, want := validToken.InstanceURL, instanceURL; got != want {
+		t.Errorf("instance url got %q want %q", got, want)
+	}
+	if got, want := fmt.Sprintf("%s", validToken.Type), "salesforce-token"; got != want {
+		t.Errorf("type got %s want %s", got, want)
+	}
+
+}
+
+// TestTokenRefresh tests that an expired token is automatically refreshed and the new
+// token returned.
+func TestTokenRefresh(t *testing.T) {
+
+	const instanceURL = "https://instance-url-example"
+	const newAccessToken = "new-token-456"
+
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	// Salesforce API authorization endpoint.
+	mux.HandleFunc("/oauth2/authorize", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if got, want := authHeader, "Bearer "+newAccessToken; got != want {
+			t.Errorf("Incorrect Authorization header got %q want %q", got, want)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		_, _ = fmt.Fprintf(w, `[{"instance_url": "%s"}]`, instanceURL)
+	})
+
+	// Salesforce API /oauth2/token refresh endpoint
+	var refreshCalled bool
+	mux.HandleFunc("/oauth2/token", func(w http.ResponseWriter, r *http.Request) {
+		refreshCalled = true
+
+		// Check that the oauth2 library is requesting a refresh correctly
+		if err := r.ParseForm(); err != nil {
+			t.Fatalf("Failed to parse form: %v", err)
+		}
+		if got := r.FormValue("grant_type"); got != "refresh_token" {
+			t.Errorf("expected grant_type refresh_token, got %s", got)
+		}
+		if got := r.FormValue("refresh_token"); got != "my-refresh-token" {
+			t.Errorf("expected refresh_token my-refresh-token, got %s", got)
+		}
+
+		// Respond with a new, valid token as JSON
+		w.Header().Set("Content-Type", "application/json")
+
+		resp := map[string]interface{}{
+			"access_token":  newAccessToken,
+			"token_type":    "Bearer",
+			"refresh_token": "new-refresh-token-if-returned",
+			// Salesforce specific fields.
+			"issued_at":    strconv.FormatInt(time.Now().UnixMilli(), 10),
+			"instance_url": instanceURL,
+			"signature":    "some-signature",
+		}
+		_ = json.NewEncoder(w).Encode(resp)
+	})
+
+	thisToken := &ExtendedToken{
+		Type:        SalesforceToken,
+		InstanceURL: instanceURL,
+		Token: &oauth2.Token{
+			AccessToken:  "expired-token-000",
+			RefreshToken: "my-refresh-token",
+			Expiry:       time.Now().Add(-1 * time.Hour), // expired
+		},
+	}
+
+	if thisToken.IsValid(duration(t, "30m")) {
+		t.Fatalf("token %#v should be invalid", thisToken)
+	}
+
+	cfg := &config.Config{
+		Salesforce: createSFConfig(t, "/callback/sf", server.URL),
+	}
+
+	// Run NewClient.
+	refreshed, err := thisToken.ReuseOrRefresh(context.Background(), cfg.Salesforce.OAuth2Config)
+	if err != nil {
+		t.Fatalf("ReuseOrRefresh returned an error: %v", err)
+	}
+	if refreshed != true {
+		t.Error("expected refresh to be true")
+	}
+	if !refreshCalled {
+		t.Error("expected token refresh endpoint to be called, but it wasn't")
+	}
+	if got, want := thisToken.InstanceURL, instanceURL; got != want {
+		t.Errorf("instance url got %q want %q", got, want)
+	}
+	if !thisToken.IsValid(duration(t, "1m")) {
+		t.Errorf("token in %#v should be valid", thisToken)
+	}
+}
+
+// TokenIsValid checks if the token (with or without refresh token) is still valid based
+// on the specified validity period. All tests run with an expected 1 hour token
+// validity.
+func TestOAuth2TokenValidity(t *testing.T) {
+
+	tests := []struct {
+		accessToken  string
+		refreshToken string
+		expiry       time.Time
+		expectedOK   bool
+	}{
+		{"001-ok-token", "", time.Now().UTC().Add(1 * time.Minute), true},
+		{"002-expired-token", "", time.Now().UTC().Add(-1 * time.Minute), false},
+		{"003-ok-token-refresh", "refresh-token", time.Now().UTC().Add(1 * time.Minute), true},
+		{"004-ok-expired-with-refresh", "refresh-token", time.Now().UTC().Add(-59 * time.Minute), true},
+		{"005-expired-with-refresh", "refresh-token", time.Now().UTC().Add(-61 * time.Minute), false},
+	}
+
+	for ii, tt := range tests {
+		t.Run(fmt.Sprintf("%d_%s", ii, tt.accessToken), func(t *testing.T) {
+			thisToken := &ExtendedToken{
+				InstanceURL: "https://an-instance.url",
+				Token: &oauth2.Token{
+					AccessToken:  tt.accessToken,
+					RefreshToken: tt.refreshToken,
+					Expiry:       tt.expiry,
+				},
+			}
+			if got, want := thisToken.IsValid(duration(t, "1h")), tt.expectedOK; got != want {
+				t.Errorf("validity check expected got %t want %t\ntoken details %v",
+					got,
+					want,
+					tokenPrinter(thisToken.Token))
+			}
+		})
+	}
+}
