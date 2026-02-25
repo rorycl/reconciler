aa52d436fa2fae77f3972f6d20b671a091412b97
rorycl <rorycl@campbell-lange.net>
2026-02-25 14:48:58 +0000

    web: make refresh capabilities web methods, more self-contained

--- web/refresh.go
+++ web/refresh.go
@@ -3,32 +3,50 @@ package web
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"reconciler/apiclients/salesforce"
 	"reconciler/apiclients/xero"
-	"reconciler/db"
-	"regexp"
+	"reconciler/internal/token"
 	"time"
 )
 
 // refreshXeroRecords retrieves the Xero organisation details, accounts, bank
-// transactions and invoices (depending on lastUpdate) and inserts these in the
-// database. If lastUpdate is time.IsZero, all records are provided. Otherwise, only
-// the invoices and bank transactions modified since lastUpdate are updated.
+// transactions and invoices (depending on lastRefresh) and inserts these in the
+// database. If lastRefresh is time.IsZero, all records are provided. Otherwise, only
+// the invoices and bank transactions modified since lastRefresh are updated.
 //
 // Information requiring to be returned can be added to the returnMap, for example
 // needed to update session information.
-func refreshXeroRecords(
-	ctx context.Context,
-	xeroClient *xero.Client,
-	db *db.DB,
-	log *slog.Logger,
-	dataStartDate,
-	lastUpdate time.Time,
-	accountsRegexp *regexp.Regexp,
-) (map[string]string, error) {
+func (web *WebApp) refreshXeroRecords(ctx context.Context) (map[string]string, error) {
 
-	fullUpdate := lastUpdate.IsZero()
+	dataStartDate := web.cfg.DataStartDate
+	accountsRegexp := web.cfg.DonationAccountCodesAsRegex()
+
+	sessDTKey := "xero-refreshed-datetime"
+	updateStart := time.Now()
+
+	// Get the last refreshed time.
+	lastRefresh := web.sessions.GetTime(ctx, sessDTKey)
+	if !lastRefresh.IsZero() {
+		lastRefresh = lastRefresh.Add(-1 * time.Minute) // window for platform updates
+	}
+	web.log.Info(fmt.Sprintf("Xero last refresh: %s", lastRefresh.Format(time.DateTime)))
+
+	// Retrieve the oauth2 tokens from the session
+	xeroToken, err := web.getValidTokenFromSession(ctx, token.XeroToken)
+	if err != nil {
+		// Todo: report errors to client.
+		return nil, fmt.Errorf("failed to refresh xero token: %v", err)
+	}
+
+	// Connect the Xero client.
+	xeroClient, err := xero.NewClient(ctx, web.log, web.cfg.DonationAccountCodesAsRegex(), xeroToken)
+	if err != nil {
+		// Todo: report errors to client.
+		return nil, fmt.Errorf("failed to create xero client: %v", err)
+	}
+	web.log.Info("Xero client authenticated successfully.")
+
+	fullUpdate := lastRefresh.IsZero()
 	returnMap := map[string]string{}
 
 	// Organisation
@@ -38,10 +56,10 @@ func refreshXeroRecords(
 		if err != nil {
 			return nil, fmt.Errorf("organisation retrieval error: %v", err)
 		}
-		if err := db.OrganisationUpsert(ctx, organisation); err != nil {
+		if err := web.db.OrganisationUpsert(ctx, organisation); err != nil {
 			return nil, fmt.Errorf("failed to upsert organisation record: %v", err)
 		}
-		log.Info("retrieved and upserted organisation record successfully")
+		web.log.Info("retrieved and upserted organisation record successfully")
 		returnMap["xero-shortcode"] = organisation.ShortCode
 	}
 
@@ -51,57 +69,81 @@ func refreshXeroRecords(
 		if err != nil {
 			return nil, fmt.Errorf("accounts retrieval error: %v", err)
 		}
-		if err := db.AccountsUpsert(ctx, accounts); err != nil {
+		if err := web.db.AccountsUpsert(ctx, accounts); err != nil {
 			return nil, fmt.Errorf("failed to upsert account records: %v", err)
 		}
-		log.Info(fmt.Sprintf("retrieved and upserted %d accounts records successfully", len(accounts)))
+		web.log.Info(fmt.Sprintf("retrieved and upserted %d accounts records successfully", len(accounts)))
 	}
 
 	// Bank Transactions
-	transactions, err := xeroClient.GetBankTransactions(ctx, dataStartDate, lastUpdate, accountsRegexp)
+	transactions, err := xeroClient.GetBankTransactions(ctx, dataStartDate, lastRefresh, accountsRegexp)
 	if err != nil {
 		return nil, fmt.Errorf("bank transaction retrieval error: %v", err)
 	}
-	if err = db.BankTransactionsUpsert(ctx, transactions); err != nil {
+	if err = web.db.BankTransactionsUpsert(ctx, transactions); err != nil {
 		return nil, fmt.Errorf("failed to upsert bank transactions: %v", err)
 	}
-	log.Info(fmt.Sprintf("retrieved and upserted %d bank transaction records successfully", len(transactions)))
+	web.log.Info(fmt.Sprintf("retrieved and upserted %d bank transaction records successfully", len(transactions)))
 
 	// Invoices
-	invoices, err := xeroClient.GetInvoices(ctx, dataStartDate, lastUpdate, accountsRegexp)
+	invoices, err := xeroClient.GetInvoices(ctx, dataStartDate, lastRefresh, accountsRegexp)
 	if err != nil {
 		return nil, fmt.Errorf("invoices retrieval error: %v", err)
 	}
-	if err := db.InvoicesUpsert(ctx, invoices); err != nil {
+	if err := web.db.InvoicesUpsert(ctx, invoices); err != nil {
 		return nil, fmt.Errorf("failed to upsert invoices: %v", err)
 	}
-	log.Info(fmt.Sprintf("retrieved and upserted %d invoice records successfully", len(invoices)))
+	web.log.Info(fmt.Sprintf("retrieved and upserted %d invoice records successfully", len(invoices)))
+
+	// Update the session key
+	web.sessions.Put(ctx, sessDTKey, updateStart)
 
 	return returnMap, nil
 }
 
 // refreshSalesforceRecords retrieves the Salesforce donation (opportunity) records. If
-// lastUpdate is time.IsZero() get all records, otherwise only get those modified since
-// lastUpdate.
-func refreshSalesforceRecords(
-	ctx context.Context,
-	sfClient *salesforce.Client,
-	db *db.DB,
-	log *slog.Logger,
-	dataStartDate,
-	lastUpdate time.Time,
-) error {
+// lastRefresh is time.IsZero() get all records, otherwise only get those modified since
+// lastRefresh.
+func (web *WebApp) refreshSalesforceRecords(ctx context.Context) error {
+
+	dataStartDate := web.cfg.DataStartDate
+
+	sessDTKey := "sf-refreshed-datetime"
+	updateStart := time.Now()
+
+	// Get the last refreshed time.
+	lastRefresh := web.sessions.GetTime(ctx, sessDTKey)
+	if !lastRefresh.IsZero() {
+		lastRefresh = lastRefresh.Add(-1 * time.Minute) // window for platform updates
+	}
+	web.log.Info(fmt.Sprintf("Salesforce last refresh: %s", lastRefresh.Format(time.DateTime)))
+
+	sfToken, err := web.getValidTokenFromSession(ctx, token.SalesforceToken)
+	if err != nil {
+		// Todo: report errors to client.
+		return fmt.Errorf("failed to refresh saleforce token: %v", err)
+	}
+
+	// Connect the Salesforce client.
+	sfClient, err := salesforce.NewClient(ctx, web.cfg, web.log, sfToken)
+	if err != nil {
+		// Todo: report errors to client.
+		return fmt.Errorf("failed to create salesforce client: %v", err)
+	}
 
 	// Donations.
-	donations, err := sfClient.GetOpportunities(ctx, dataStartDate, lastUpdate)
+	donations, err := sfClient.GetOpportunities(ctx, dataStartDate, lastRefresh)
 	if err != nil {
 		return fmt.Errorf("failed to retrieve donations: %v", err)
 	}
 
-	if err := db.UpsertDonations(ctx, donations); err != nil {
+	if err := web.db.UpsertDonations(ctx, donations); err != nil {
 		return fmt.Errorf("failed to upsert donations: %v", err)
 	}
-	log.Info(fmt.Sprintf("retrieved and upserted %d donations records successfully", len(donations)))
+	web.log.Info(fmt.Sprintf("retrieved and upserted %d donations records successfully", len(donations)))
+
+	// Update the session key
+	web.sessions.Put(ctx, sessDTKey, updateStart)
 
 	return nil
 }
