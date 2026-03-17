7362b52989dcbf610bc93c852bac573afa283c30
rorycl <rorycl@campbell-lange.net>
2026-03-17 22:48:20 +0000

    web: rework types for domain structure (wip)

--- web/types.go
+++ web/types.go
@@ -5,43 +5,69 @@ package web
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"regexp"
+
 	"github.com/rorycl/reconciler/apiclients/salesforce"
 	"github.com/rorycl/reconciler/apiclients/xero"
 	"github.com/rorycl/reconciler/config"
+	"github.com/rorycl/reconciler/domain"
 	"github.com/rorycl/reconciler/internal/token"
-	"log/slog"
-	"regexp"
-	"time"
 )
-
-// xeroClienter is an interface to the capabilities of a xero API client.
-type xeroClienter interface {
-	GetOrganisation(ctx context.Context) (xero.Organisation, error)
-	GetAccounts(ctx context.Context, ifModifiedSince time.Time) ([]xero.Account, error)
-	GetBankTransactions(ctx context.Context, fromDate time.Time, ifModifiedSince time.Time, accountsRegexp *regexp.Regexp) ([]xero.BankTransaction, error)
-	GetInvoices(ctx context.Context, fromDate time.Time, ifModifiedSince time.Time, accountsRegexp *regexp.Regexp) ([]xero.Invoice, error)
-}
 
 // NewXeroClienter is a factory function for returning the default xeroClient as an
 // xeroClienter
-func newXeroClienter(ctx context.Context, logger *slog.Logger, accountsRegexp *regexp.Regexp, et *token.ExtendedToken) (xeroClienter, error) {
+func newXeroClientMaker(ctx context.Context, logger *slog.Logger, accountsRegexp *regexp.Regexp, et *token.ExtendedToken) (domain.XeroClient, error) {
 	return xero.NewClient(ctx, logger, accountsRegexp, et)
 }
 
 // xeroFactoryFunc is the signature of newXeroClienter.
-type xeroFactoryFunc func(ctx context.Context, logger *slog.Logger, accountsRegexp *regexp.Regexp, et *token.ExtendedToken) (xeroClienter, error)
-
-// sfClienter is an interface to the capabilities of a saleforce API client.
-type sfClienter interface {
-	BatchUpdateOpportunityRefs(ctx context.Context, idRefs []salesforce.IDRef, allOrNone bool) (salesforce.CollectionsUpdateResponse, error)
-	GetOpportunities(ctx context.Context, fromDate, ifModifiedSince time.Time) ([]salesforce.Donation, error)
-}
+type xeroClientMaker func(ctx context.Context, logger *slog.Logger, accountsRegexp *regexp.Regexp, et *token.ExtendedToken) (domain.XeroClient, error)
 
 // NewSalesforceClienter is a factory function for returning the default sfClient as an
-// sfClienter.
-func newSalesforceClienter(ctx context.Context, cfg *config.Config, logger *slog.Logger, et *token.ExtendedToken) (sfClienter, error) {
+// sfClientMaker.
+func newSalesforceClientMaker(ctx context.Context, cfg *config.Config, logger *slog.Logger, et *token.ExtendedToken) (domain.SalesforceClient, error) {
 	return salesforce.NewClient(ctx, cfg, logger, et)
 }
 
 // sfFactoryFunc is the signature of newXeroClienter.
-type sfFactoryFunc func(ctx context.Context, cfg *config.Config, logger *slog.Logger, et *token.ExtendedToken) (sfClienter, error)
+type sfClientMaker func(ctx context.Context, cfg *config.Config, logger *slog.Logger, et *token.ExtendedToken) (domain.SalesforceClient, error)
+
+// appHandler is a type of handler that returns an error. All normal web handlers are
+// appHandlers and are wrapped by ErrorChecker which centralises error reporting.
+type appHandler func(http.ResponseWriter, *http.Request) error
+
+// errInternal is an internal error that should trigger a 500 response. The err should
+// only be logged, not reported to the client.
+type errInternal struct {
+	msg string
+	err error
+}
+
+func (ei errInternal) Error() string {
+	return fmt.Sprintf("%s: %s", ei.msg, ei.err)
+}
+
+// errUsage is a usage error that should trigger a 4xx response. The required status is
+// noted in the status field.
+type errUsage struct {
+	msg    string
+	status int
+}
+
+func (eu errUsage) Error() string {
+	return fmt.Sprintf("%s (%d)", eu.msg, eu.status)
+}
+
+// errHTMX is an error to send back to the htmx front end. The error should be logged
+// but not shown to the client -- use msg for that.
+type errHTMX struct {
+	msg string
+	err error
+}
+
+func (eh errHTMX) Error() string {
+	return fmt.Sprintf("%s: %v", eh.msg, eh.err)
+}
