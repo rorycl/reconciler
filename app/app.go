ea5e40e6fb825e30d494ab485b26a790fcd62d35
rorycl <rorycl@campbell-lange.net>
2026-03-21 20:58:29 +0000

    app: plug in filewatcher for indevelopment reloading

--- app/app.go
+++ app/app.go
@@ -3,29 +3,36 @@
 package app
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io/fs"
 	"log/slog"
 	"os"
+	"path/filepath"
 
 	"github.com/rorycl/reconciler/config"
 	"github.com/rorycl/reconciler/db"
 	"github.com/rorycl/reconciler/domain"
+	"github.com/rorycl/reconciler/internal/filewatcher"
 	mounts "github.com/rorycl/reconciler/internal/mounts"
 	"github.com/rorycl/reconciler/web"
 )
 
-// App encapsulates the functions of reconciler for use by command programs.
+// App encapsulates a reconciler and associated infrastructure elements for use by
+// command programs.
 type App struct {
-	cfg           *config.Config
-	log           *slog.Logger
-	logLevel      slog.Level
+	cfg        *config.Config
+	log        *slog.Logger
+	logLevel   slog.Level
+	reconciler *domain.Reconciler
+	staticFS   fs.FS
+	templateFS fs.FS
+	sqlFS      fs.FS
+
+	// development fields
 	inDevelopment bool
-	reconciler    *domain.Reconciler
-	staticFS      fs.FS
-	templateFS    fs.FS
-	sqlFS         fs.FS
+	watcher       <-chan error
 }
 
 // NewApp initialises a new App.
@@ -93,7 +100,7 @@ func NewApp(
 	// Construct the reconciler
 	reconciler := domain.NewReconciler(dbCon, logger)
 
-	return &App{
+	app := &App{
 		cfg:           cfg,
 		log:           logger,
 		logLevel:      logLevel,
@@ -102,7 +109,26 @@ func NewApp(
 		staticFS:      staticFS,
 		templateFS:    templateFS,
 		sqlFS:         sqlFS,
-	}, nil
+	}
+
+	// Register filewatchers if in development, for automatic reloading.
+	if inDevelopment {
+		watcher, err := filewatcher.NewFileChangeNotifier(
+			context.Background(),
+			map[string][]string{
+				filepath.Join(staticPath, "css"): {".css"},
+				filepath.Join(staticPath, "js"):  {".js"},
+				templatePath:                     {".html"},
+				sqlPath:                          {".sql"},
+			},
+		)
+		if err != nil {
+			return nil, fmt.Errorf("file watcher error: %v", err)
+		}
+		app.watcher = watcher.Update()
+	}
+
+	return app, nil
 
 }
 
@@ -116,8 +142,30 @@ func (a *App) RunWebServer() error {
 		a.log.Error(fmt.Sprintf("app web server init error: %v", err))
 		return fmt.Errorf("could not initialise web server: %w", err)
 	}
+
+	// If inDevelopment mode, set the webapp in development, and run the file watcher in
+	// a goroutine to range over events to either deal with errors or trigger route
+	// restarting (which recompiles the templates) if file changes are detected.
 	if a.inDevelopment {
+
 		webApp.SetInDevelopment()
+
+		go func() {
+			for err := range a.watcher {
+				if err != nil {
+					a.log.Error(fmt.Sprintf("file watcher error: %v", err))
+					a.log.Error("file watching has stopped")
+					return
+				}
+				a.log.Info("file watcher detected a file change; restarting routes")
+
+				// Restart the web server routes.
+				webApp.RestartRoutes()
+			}
+		}()
 	}
+
+	// Start the server.
 	return webApp.StartServer()
+
 }
