c30f3736eb9eb67819c8a508e638c58e481deb1f
rorycl <rorycl@campbell-lange.net>
2026-03-23 20:54:19 +0000

    cmd: data validate and iterate salesforce excel records

---
+++ cmd/sfbatcher/data.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"iter"
+	"strings"
+
+	"github.com/rorycl/reconciler/apiclients/salesforce"
+)
+
+// Parser is an interface met by an ExcelParser.
+type Parser interface {
+	Headers() []string
+	Rows() iter.Seq[[]string]
+}
+
+// Data represents the validated contents of a spreadsheet describing a set of unique
+// salesforce IDs whose reference field (as described by the associated config file)
+// should be updated with the associated reference. Note that for linking actions the
+// reference ("Ref" part of "IDRef") will be empty.
+type Data struct {
+	idRefs   []salesforce.IDRef
+	uniqueID map[string]struct{}
+	Records  int
+}
+
+// NewData initialises a new Data and fills it with content from the provided parser.
+func NewData(parser Parser) (*Data, error) {
+	data := &Data{
+		idRefs:   []salesforce.IDRef{},
+		uniqueID: map[string]struct{}{},
+	}
+	header := parser.Headers()
+	if err := data.validHeaders(header); err != nil {
+		return data, err
+	}
+	data.Records = 1
+	for row := range parser.Rows() {
+		idRef, err := data.validateRow(row)
+		if err != nil {
+			return data, fmt.Errorf("row %d (1 indexed) error: %w", data.Records, err)
+		}
+		data.idRefs = append(data.idRefs, idRef)
+		data.Records++
+	}
+	return data, nil
+}
+
+// validHeaders ensures that the Excel file headers match the
+//
+//	`ID | Ref | ...`
+//
+// pattern.
+func (d *Data) validHeaders(headers []string) error {
+	if len(headers) < 2 {
+		return errors.New("at least two headers -- ID and Ref -- expected")
+	}
+	if strings.TrimSpace(strings.ToLower(headers[0])) != "id" {
+		return fmt.Errorf("header %q found -- expected ID", headers[0])
+	}
+	if strings.TrimSpace(strings.ToLower(headers[1])) != "ref" {
+		return fmt.Errorf("header %q found -- expected ref", headers[1])
+	}
+	return nil
+}
+
+// validateRow validates the contents of an Excel Row, ensuring the contents are valid
+// and unique Salesforce IDs.
+func (d *Data) validateRow(row []string) (salesforce.IDRef, error) {
+
+	idRef := salesforce.IDRef{}
+
+	if len(row) < 2 {
+		return idRef, errors.New("row did not have at least 2 columns")
+	}
+	idRef = salesforce.IDRef{row[0], row[1]}
+	if err := salesforce.IDsValid(idRef.ID); err != nil {
+		return idRef, err
+	}
+	if _, exists := d.uniqueID[idRef.ID]; exists {
+		return idRef, fmt.Errorf("duplicate ID %q found", idRef.ID)
+	}
+	d.uniqueID[idRef.ID] = struct{}{}
+	return idRef, nil
+}
+
+// Batch returns the data.idRefs in slices no larger than batchsize.
+func (d *Data) Batch(batchsize int) iter.Seq[[]salesforce.IDRef] {
+	refLen := len(d.idRefs)
+	return func(yield func([]salesforce.IDRef) bool) {
+		if d.Records == 0 {
+			return
+		}
+		start, end := 0, batchsize
+		for {
+			if end > refLen {
+				end = refLen
+			}
+			if !yield(d.idRefs[start:end]) {
+				return
+			}
+			start += batchsize
+			end += batchsize
+			if start >= refLen {
+				return
+			}
+		}
+	}
+}
---
+++ cmd/sfbatcher/data_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"iter"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/rorycl/reconciler/apiclients/salesforce"
+)
+
+func generate(d *Data, no int, t *testing.T) {
+	t.Helper()
+	counter := 0
+	d.idRefs = make([]salesforce.IDRef, no)
+	for range no {
+		d.idRefs[counter] = salesforce.IDRef{strconv.Itoa(counter), "hi"}
+		counter++
+	}
+	d.Records = no
+}
+
+func TestDataBatch(t *testing.T) {
+	for ii, tt := range []struct {
+		inputNo      int
+		expectedRows int
+	}{
+		{7, 4},
+		{6, 3},
+		{0, 0},
+	} {
+		t.Run(fmt.Sprintf("test_%d", ii), func(t *testing.T) {
+			d := &Data{}
+			generate(d, tt.inputNo, t)
+			rows := 0
+			for range d.Batch(2) {
+				rows++
+			}
+			if got, want := rows, tt.expectedRows; got != want {
+
+				t.Errorf("got %d want %d rows", got, want)
+			}
+		})
+	}
+}
+
+type pok struct{}
+
+func (p *pok) Headers() []string { return []string{"ID", "Ref"} }
+func (p *pok) Rows() iter.Seq[[]string] {
+	return func(yield func([]string) bool) {
+		for _, r := range [][]string{{"0033000000abcde", "1"}, {"0063000000abcde", "2"}} {
+			if !yield(r) {
+				return
+			}
+		}
+	}
+}
+
+type pbad struct{ headersBad bool }
+
+func (p *pbad) Headers() []string {
+	if !p.headersBad {
+		return []string{"ID", "Ref"} // ok headers
+	} else {
+		return []string{"invalid", "Ref"} // invalid headers
+	}
+}
+func (p *pbad) Rows() iter.Seq[[]string] {
+	return func(yield func([]string) bool) {
+		for _, r := range [][]string{{"tooshort", "1"}} {
+			if !yield(r) {
+				return
+			}
+		}
+	}
+}
+
+func TestDataNew(t *testing.T) {
+
+	tests := []struct {
+		parser Parser
+		isErr  bool
+		errMsg string
+	}{
+		{&pok{}, false, ""},
+		{&pbad{false}, true, "row 1 (1 indexed) error: \"tooshort\" is an invalid Salesforce ID"},
+		{&pbad{true}, true, "header \"invalid\" found -- expected ID"},
+	}
+	for ii, tt := range tests {
+		t.Run(fmt.Sprintf("test_%d", ii), func(t *testing.T) {
+
+			_, err := NewData(tt.parser)
+
+			if err != nil && !tt.isErr {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if err == nil && tt.isErr {
+				t.Fatal("expected an error")
+			}
+			if err == nil {
+				return
+			}
+			if got, want := err.Error(), tt.errMsg; !strings.Contains(got, want) {
+				t.Errorf("got error %q did not contain %q", got, want)
+			}
+
+		})
+	}
+}
